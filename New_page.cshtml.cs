6d67209c2a4191f07501b2c9fa8b4a9517ef7c9c
Yash <yashkelkar09@gmail.com>
2022-02-22 16:27:04 -0500

    First commit

---
+++ New_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Intro.Pages
+{
+    public class New_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
